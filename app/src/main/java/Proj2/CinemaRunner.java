49d14bb31144ea55d4489fb70c4292484aac5e14
Eric <5cminsuhlim@gmail.com>
2021-10-18 12:20:43 +1100

    update CinemaRunner.java
    -add switch cases for guest booking

--- app/src/main/java/Proj2/CinemaRunner.java
+++ app/src/main/java/Proj2/CinemaRunner.java
@@ -49,28 +49,55 @@ public class CinemaRunner {
 
         //guest function
         while(running){
+            //prompt guest to search for movie or filter cinema
             String guestChoice = u.promptGuest();
 
             switch(guestChoice){
                 case "1":
-                    String guestMovie = u.findMovie();
+                    //prompt guest to look up a movie
+                    guestChoice = u.findMovie();
 
                     for(Cinema c : validCinemas) {
                         for(Movie m : c.getMovies()){
-                            if(m.getName().equals(guestMovie)){
+                            //if movie is found
+                            if(m.getName().equals(guestChoice)){
+                                //print details
                                 m.getMovieDetails();
+
+                                //prompt guest if they want to book
+                                guestChoice = u.bookMovie();
+
+                                switch(guestChoice){
+                                    case "1":
+                                        //prompt guest to make an account / log in
+                                        System.out.println("To proceed with booking, please make an account or log in.\n");
+
+                                    case "2":
+                                        //don't book
+                                        break;
+
+                                    default:
+                                        System.out.println("Invalid Input, please try again.\n");
+                                }
+                            }
+                            else{
+                                System.out.println("Movie not found\n");
                             }
                         }
                     }
                 case "2":
-                    String guestCinema = u.findCinema();
+                    //prompt guest to filter by cinema
+                    guestChoice = u.findCinema();
 
                     for(Cinema c : validCinemas) {
-                        if(c.getName().equals(guestCinema)){
+                        if(c.getName().equals(guestChoice)){
                             for(Movie m : c.getMovies()){
                                 System.out.println(m.getName());
                                 System.out.println(m.getSchedule());
                             }
+                        }
+                        else{
+                            System.out.println("Cinema not found\n");
                         }
                     }
                 default:
